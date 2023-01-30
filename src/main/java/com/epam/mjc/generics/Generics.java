ff3db96037ae8611fb9ee39b71226095ac2a63c3
abboskhan <abbosxon28012004@gmail.com>
2023-01-30 15:03:00 +0500

    ending

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -23,7 +23,7 @@ public class Generics {
     }
 
     //TODO: Refactor Method-3
-    public <T extends Integer>void cloneMethod(List<? super T> consumer, List<? extends T> producer) {
+    public <T>void cloneMethod(List<? super T> consumer, List<? extends T> producer) {
         consumer.addAll(producer);
     }
 
